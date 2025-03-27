17d3188027f6dcb6490753692c93aec61152bbff
Vinh Vo <vohuynhvinh@gmail.com>
2025-03-27 21:46:10 +0700

    Switch to use lexical

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,172 +1,169 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
+import {LexicalComposer} from '@lexical/react/LexicalComposer';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {
-    AdmonitionDirectiveDescriptor,
-    BlockTypeSelect,
-    BoldItalicUnderlineToggles,
-    codeBlockPlugin,
-    codeMirrorPlugin,
-    CodeToggle,
-    CreateLink,
-    diffSourcePlugin,
-    DiffSourceToggleWrapper,
-    directivesPlugin,
-    frontmatterPlugin,
-    headingsPlugin,
-    InsertAdmonition,
-    InsertCodeBlock,
-    InsertImage,
-    InsertTable,
-    InsertThematicBreak,
-    jsxPlugin,
-    linkDialogPlugin,
-    linkPlugin,
-    listsPlugin,
-    ListsToggle,
-    markdownShortcutPlugin,
-    MDXEditor,
-    quotePlugin,
-    tablePlugin,
-    thematicBreakPlugin,
-    toolbarPlugin,
-    UndoRedo,
-} from "@mdxeditor/editor";
-import "@mdxeditor/editor/style.css";
+    $isTextNode,
+    DOMConversionMap,
+    DOMExportOutput,
+    DOMExportOutputMap,
+    isHTMLElement,
+    Klass,
+    LexicalEditor,
+    LexicalNode,
+    ParagraphNode,
+    TextNode,
+} from 'lexical';
+
+import defaultTheme from './theme';
+import ToolbarPlugin from './plugins/ToolbarPlugin';
+import TreeViewPlugin from './plugins/TreeViewPlugin';
+import {parseAllowedColor, parseAllowedFontSize} from './styleConfig';
+
+const placeholder = 'Enter some rich text...';
+
+const removeStylesExportDOM = (
+    editor: LexicalEditor,
+    target: LexicalNode,
+): DOMExportOutput => {
+    const output = target.exportDOM(editor);
+    if (output && isHTMLElement(output.element)) {
+        // Remove all inline styles and classes if the element is an HTMLElement
+        // Children are checked as well since TextNode can be nested
+        // in i, b, and strong tags.
+        for (const el of [
+            output.element,
+            ...output.element.querySelectorAll('[style],[class],[dir="ltr"]'),
+        ]) {
+            el.removeAttribute('class');
+            el.removeAttribute('style');
+            if (el.getAttribute('dir') === 'ltr') {
+                el.removeAttribute('dir');
+            }
+        }
+    }
+    return output;
+};
+
+const exportMap: DOMExportOutputMap = new Map<
+    Klass<LexicalNode>,
+    (editor: LexicalEditor, target: LexicalNode) => DOMExportOutput
+>([
+    [ParagraphNode, removeStylesExportDOM],
+    [TextNode, removeStylesExportDOM],
+]);
+
+const getExtraStyles = (element: HTMLElement): string => {
+    // Parse styles from pasted input, but only if they match exactly the
+    // sort of styles that would be produced by exportDOM
+    let extraStyles = '';
+    const fontSize = parseAllowedFontSize(element.style.fontSize);
+    const backgroundColor = parseAllowedColor(element.style.backgroundColor);
+    const color = parseAllowedColor(element.style.color);
+    if (fontSize !== '' && fontSize !== '15px') {
+        extraStyles += `font-size: ${fontSize};`;
+    }
+    if (backgroundColor !== '' && backgroundColor !== 'rgb(255, 255, 255)') {
+        extraStyles += `background-color: ${backgroundColor};`;
+    }
+    if (color !== '' && color !== 'rgb(0, 0, 0)') {
+        extraStyles += `color: ${color};`;
+    }
+    return extraStyles;
+};
+
+const constructImportMap = (): DOMConversionMap => {
+    const importMap: DOMConversionMap = {};
+
+    // Wrap all TextNode importers with a function that also imports
+    // the custom styles implemented by the playground
+    for (const [tag, fn] of Object.entries(TextNode.importDOM() || {})) {
+        importMap[tag] = (importNode) => {
+            const importer = fn(importNode);
+            if (!importer) {
+                return null;
+            }
+            return {
+                ...importer,
+                conversion: (element) => {
+                    const output = importer.conversion(element);
+                    if (
+                        output === null ||
+                        output.forChild === undefined ||
+                        output.after !== undefined ||
+                        output.node !== null
+                    ) {
+                        return output;
+                    }
+                    const extraStyles = getExtraStyles(element);
+                    if (extraStyles) {
+                        const {forChild} = output;
+                        return {
+                            ...output,
+                            forChild: (child, parent) => {
+                                const textNode = forChild(child, parent);
+                                if ($isTextNode(textNode)) {
+                                    textNode.setStyle(textNode.getStyle() + extraStyles);
+                                }
+                                return textNode;
+                            },
+                        };
+                    }
+                    return output;
+                },
+            };
+        };
+    }
+
+    return importMap;
+};
+
+const editorConfig = {
+    html: {
+        export: exportMap,
+        import: constructImportMap(),
+    },
+    namespace: 'React.js Demo',
+    nodes: [ParagraphNode, TextNode],
+    onError(error: Error) {
+        throw error;
+    },
+    theme: defaultTheme,
+};
 
 export default function App() {
-    const markdown = `# Welcome
-
-This is a **live demo** of MDXEditor with all default features on.
-
-> The overriding design goal for Markdown’s formatting syntax is to make it as readable as possible.
-> The idea is that a Markdown-formatted document should be publishable as-is, as plain text,
-> without looking like it’s been marked up with tags or formatting instructions.
-
-[— Daring Fireball](https://daringfireball.net/projects/markdown/).
-
-In here, you can find the following markdown elements:
-
-* Headings
-* Lists
-  * Unordered
-  * Ordered
-  * Check lists
-  * And nested ;)
-* Links
-* Bold/Italic/Underline formatting
-* Tables
-* Code block editors
-* And much more.
-
-The current editor content is styled using the \`@tailwindcss/typography\` [plugin](https://tailwindcss.com/docs/typography-plugin).
-
-## What can you do here?
-
-This is a great location for you to test how editing markdown feels. If you have an existing markdown source, you can switch to source mode using the toggle group in the top right, paste it in there, and go back to rich text mode.
-
-If you need a few ideas, here's what you can try:
-
-1. Add your own code sample
-2. Change the type of the headings
-3. Insert a table, add a few rows and columns
-4. Switch back to source markdown to see what you're going to get as an output
-5. Test the diff feature to see how the markdown has changed
-6. Add a frontmatter block through the toolbar button
-
-## A code sample
-
-MDXEditor embeds CodeMirror for code editing.
-
-\`\`\`tsx
-let x = 10;
-x. = 20: 11 Works fine
-\`\`\`
-
-## A live code example
-
-The block below is a live React component. You can configure multiple live code presets that specify the available npm packages and the default imports. You can also specify a default component that will be rendered in the live code block.
-
-\`\`\`tsx
-let x = 10;
-x. = 20: 11 Works fine
-\`\`\`
-
-\`\`\`tsx
-export default function App() {
-  return (<div>
-  <p>This is a live React component, that's being previewed in codesandbox. </p>
-  <p>Editing it will update the fenced codeblock in the markdown.</p>
-  </div>)
-}
-\`\`\`
-
-\`\`\`tsx
-function( x: int ) { return x*x; }
-\`\`\`
-
-## A table
-
-Play with the table below - add rows, columns, change column alignment. When editing,
-you can navigate the cells with \`enter\`, \`shift+enter\`, \`tab\` and \`shift+tab\`.
-
-| Item              | In Stock | Price |
-| :---------------- | :------: | ----: |
-| Python Hat        |   True   | 23.99 |
-| SQL Hat           |   True   | 23.99 |
-| Codecademy Tee    |   False  | 19.99 |
-| Codecademy Hoodie |   False  | 42.99 |
-`;
-
     return (
-        <div>
-            <MDXEditor
-                markdown={markdown}
-                plugins={[
-                    diffSourcePlugin({
-                        diffMarkdown: markdown,
-                        viewMode: "diff",
-                    }),
-                    frontmatterPlugin(),
-                    jsxPlugin(),
-                    codeBlockPlugin({
-                        defaultCodeBlockLanguage: "tsx",
-                    }),
-                    codeMirrorPlugin({
-                        codeBlockLanguages: {
-                            js: "JavaScript",
-                            css: "CSS",
-                            txt: "text",
-                            tsx: "TypeScript",
-                        },
-                    }),
-                    directivesPlugin({
-                        directiveDescriptors: [AdmonitionDirectiveDescriptor],
-                    }),
-                    headingsPlugin(),
-                    listsPlugin(),
-                    linkPlugin(),
-                    linkDialogPlugin(),
-                    tablePlugin(),
-                    quotePlugin(),
-                    thematicBreakPlugin(),
-                    toolbarPlugin({
-                        toolbarContents: () => (
-                            <DiffSourceToggleWrapper>
-                                <BlockTypeSelect />
-                                <BoldItalicUnderlineToggles />
-                                <CodeToggle />
-                                <CreateLink />
-                                <InsertAdmonition />
-                                <InsertCodeBlock />
-                                <InsertImage />
-                                <InsertTable />
-                                <InsertThematicBreak />
-                                <ListsToggle />
-                                <UndoRedo />
-                            </DiffSourceToggleWrapper>
-                        ),
-                    }),
-                    markdownShortcutPlugin(),
-                ]}
-            />
-        </div>
+        <LexicalComposer initialConfig={editorConfig}>
+            <div className="editor-container">
+                <ToolbarPlugin />
+                <div className="editor-inner">
+                    <RichTextPlugin
+                        contentEditable={
+                            <ContentEditable
+                                className="editor-input"
+                                aria-placeholder={placeholder}
+                                placeholder={
+                                    <div className="editor-placeholder">{placeholder}</div>
+                                }
+                            />
+                        }
+                        ErrorBoundary={LexicalErrorBoundary}
+                    />
+                    <HistoryPlugin />
+                    <AutoFocusPlugin />
+                    <TreeViewPlugin />
+                </div>
+            </div>
+        </LexicalComposer>
     );
 }
